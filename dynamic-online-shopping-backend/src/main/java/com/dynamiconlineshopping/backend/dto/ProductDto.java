349a3292bc3ba17dffd11ed9c1bf170f7d6cc4a1
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 23:50:19 +0530

    Add ProductDto

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/dto/ProductDto.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/dto/ProductDto.java
@@ -1,4 +1,21 @@
 package com.dynamiconlineshopping.backend.dto;
 
+import lombok.*;
+
+/**
+ * ProductDto - DTO for API responses/requests.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String sku;
+    private String imageUrl;
 }
