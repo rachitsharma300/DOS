3f5ad42ce191dde1e3fa4117cab8172560e178f0
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 23:52:25 +0530

    Add AuthService interface

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/service/AuthService.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/service/AuthService.java
@@ -1,4 +1,13 @@
 package com.dynamiconlineshopping.backend.service;
 
-public class AuthService {
+import com.dynamiconlineshopping.backend.dto.AuthRequest;
+import com.dynamiconlineshopping.backend.dto.RegisterRequest;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * AuthService - authentication and registration.
+ */
+public interface AuthService extends UserDetailsService {
+    Object register(RegisterRequest request);
+    Object login(AuthRequest request);
 }
