1429776eae2091bcbda026f0691c9cac51291e2d
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 16:03:51 +0530

    Initial commit DOS Backend && DOS Frontend

---
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/DynamicOnlineShoppingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dynamiconlineshopping.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicOnlineShoppingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicOnlineShoppingBackendApplication.class, args);
+	}
+
+}
---
+++ dynamic-online-shopping-backend/src/test/java/com/dynamiconlineshopping/backend/DynamicOnlineShoppingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamiconlineshopping.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicOnlineShoppingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
