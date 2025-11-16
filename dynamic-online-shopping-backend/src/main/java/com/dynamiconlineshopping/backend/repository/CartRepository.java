48472da223f17c110305c409ce312bc30630bd11
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 23:52:04 +0530

    Add CartRepository

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/CartRepository.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/CartRepository.java
@@ -1,4 +1,15 @@
 package com.dynamiconlineshopping.backend.repository;
 
-public class CartRepository {
+import com.dynamiconlineshopping.backend.entity.CartItem;
+import com.dynamiconlineshopping.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * CartRepository.
+ */
+public interface CartRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUser(User user);
+    void deleteByUserAndProductId(User user, Long productId);
 }
