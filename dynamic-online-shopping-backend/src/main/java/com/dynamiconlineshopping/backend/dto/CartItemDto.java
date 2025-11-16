976abae46fbd660f55a63afc5649a5b74b51280d
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 23:50:33 +0530

    Add CartItemDto

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/dto/CartItemDto.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/dto/CartItemDto.java
@@ -1,4 +1,18 @@
 package com.dynamiconlineshopping.backend.dto;
 
+import lombok.*;
+
+/**
+ * CartItemDto - for cart operations.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CartItemDto {
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+    private ProductDto product; // optional snapshot
 }
