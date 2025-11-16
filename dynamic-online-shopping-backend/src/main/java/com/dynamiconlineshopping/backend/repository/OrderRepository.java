e132ea322be3d487fb70469eeb765e953d270426
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-17 01:56:42 +0530

    feat: Create Order repository with custom queries

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/OrderRepository.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/OrderRepository.java
@@ -5,10 +5,9 @@ import com.dynamiconlineshopping.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-/**
- * OrderRepository.
- */
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser(User user);
+    Optional<Order> findByRazorpayOrderId(String razorpayOrderId); // ADD THIS METHOD
 }
