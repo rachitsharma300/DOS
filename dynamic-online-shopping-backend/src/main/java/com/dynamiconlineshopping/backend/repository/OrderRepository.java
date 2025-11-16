cb90b553953907b27fdb300893dc0a082c491656
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 23:52:12 +0530

    Add OrderRepository

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/OrderRepository.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/OrderRepository.java
@@ -1,4 +1,14 @@
 package com.dynamiconlineshopping.backend.repository;
 
-public class OrderRepository {
+import com.dynamiconlineshopping.backend.entity.Order;
+import com.dynamiconlineshopping.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * OrderRepository.
+ */
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser(User user);
 }
