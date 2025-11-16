11663c277654960a60c5291f90bfa1087a705c90
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-16 23:51:51 +0530

    Add ProductRepository

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/ProductRepository.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.dynamiconlineshopping.backend.repository;
 
-public class ProductRepository {
+import com.dynamiconlineshopping.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * ProductRepository.
+ */
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
