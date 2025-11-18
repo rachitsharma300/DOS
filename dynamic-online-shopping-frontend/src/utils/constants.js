085aff7554e5cd5e2721bb363201cc3233ced183
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-18 14:55:30 +0530

    config: place base URLs & payment constants

---
+++ dynamic-online-shopping-frontend/src/utils/constants.js
@@ -0,0 +1,13 @@
+export const API_BASE_URL = "http://localhost:8080/api";
+
+export const ROUTES = {
+  HOME: "/",
+  PRODUCTS: "/products",
+  CART: "/cart",
+  ORDERS: "/orders",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  PROFILE: "/profile",
+};
+
+export const CURRENCY_SYMBOL = "₹";
