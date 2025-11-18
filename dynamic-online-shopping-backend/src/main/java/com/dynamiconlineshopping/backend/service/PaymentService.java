be7ddef25aa375afd229af5100c6f93afdbdf47a
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-18 14:45:19 +0530

    feat: define payment workflow interface & rules

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/service/PaymentService.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/service/PaymentService.java
@@ -1,9 +1,11 @@
 package com.dynamiconlineshopping.backend.service;
 
+import java.util.Map;
+
 /**
  * PaymentService - handles payment gateway integration.
  */
 public interface PaymentService {
     Object createRazorpayOrder(Long orderId);
-    Object verifyPayment(String payload);
+    Object verifyPayment(Map<String, String> payload);
 }
