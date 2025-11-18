ecb77afd008513c4328fcff8d03140764ea1d3f6
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-18 14:44:54 +0530

    feat: add repository methods for authentication & profile operations

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/UserRepository.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dynamiconlineshopping.backend.repository;
 
 import com.dynamiconlineshopping.backend.entity.User;
+import com.dynamiconlineshopping.backend.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.Optional;
  */
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    // METHOD FOR ADMIN
+    long countByRole(Role role);
 }
