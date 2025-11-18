e0911afdaf4323a3f6a624c484fda42d58049fc4
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-18 14:44:06 +0530

    feat: create repository for order entity operations

--- dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/OrderRepository.java
+++ dynamic-online-shopping-backend/src/main/java/com/dynamiconlineshopping/backend/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.dynamiconlineshopping.backend.repository;
 
 import com.dynamiconlineshopping.backend.entity.Order;
 import com.dynamiconlineshopping.backend.entity.User;
+import com.dynamiconlineshopping.backend.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,9 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUser(User user);
-    Optional<Order> findByRazorpayOrderId(String razorpayOrderId); // ADD THIS METHOD
+    Optional<Order> findByRazorpayOrderId(String razorpayOrderId);
+
+    // METHODS FOR ADMIN
+    List<Order> findByStatus(OrderStatus status);
+    List<Order> findTop5ByOrderByCreatedAtDesc();
 }
